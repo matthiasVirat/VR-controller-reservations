46e7146598cea3da95df7f3a3c00c59250a3d36b
matthiasVirat <virat.matthias@gmail.com>
2019-10-28 10:07:10 +0100

    initial commit

---
+++ src/main/java/com/vehiculerental/controllerreservations/ControllerreservationsApplication.java
@@ -0,0 +1,13 @@
+package com.vehiculerental.controllerreservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerreservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllerreservationsApplication.class, args);
+	}
+
+}
